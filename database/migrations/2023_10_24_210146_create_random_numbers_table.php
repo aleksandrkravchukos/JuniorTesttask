6bd84a121c8bb836d71685d69689c07ca80766a5
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-10-25 12:49:02 +0300

    Fix.

--- database/migrations/2023_10_24_210146_create_random_numbers_table.php
+++ database/migrations/2023_10_24_210146_create_random_numbers_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('random_numbers', function (Blueprint $table) {
             $table->id();
             $table->string('number');
-            $table->string('uuid')->unique();
             $table->timestamps();
         });
     }
